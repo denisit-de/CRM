ae27f5f5c1f941f1812216877efaf972ee94222b
Denis IT <mail@denisit.de>
2023-09-10 16:06:45 +0200

    added (identity) user and sqlite db

---
+++ Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Api.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+	public class AppDbContext : IdentityDbContext<User>
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+	}
+}
+
---
+++ Api/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Data.Models
+{
+	public class User : IdentityUser
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public DateOnly DateCreated { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+	}
+}
+
