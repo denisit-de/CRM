3fc09a830e534e4cb48d216cef40bc8c9920d59b
Denis IT <mail@denisit.de>
2023-09-22 20:15:16 +0200

    implemented validations & error-handling for login

--- Client/src/app/account/login/login.component.ts
+++ Client/src/app/account/login/login.component.ts
@@ -11,25 +11,37 @@ import { User } from 'src/app/models/account/User';
 export class LoginComponent {
 
   form: FormGroup;
+  errorMessages: string[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
-    private accountService: AccountService) {
+    private accountService: AccountService,
+    ) {
     this.form = formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
   onSubmit(): void {
     this.accountService.login(this.form.value).subscribe({
       next: data => this.onLogin(data),
-      error: err => console.log(err)
+      error: err => this.onError(err)
     });
   }
 
   private onLogin(data: User): void {
+    this.errorMessages = [];
     console.log(data);
+  }
+
+  private onError(err: any): void {
+    if(err.errors) {
+      this.errorMessages = err.errors;
+    } else {
+      this.errorMessages = [];
+      this.errorMessages.push(err.error);
+    }
   }
 
 }
